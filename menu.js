82e71db77ebdd5ed841cf096ffddefd2e1f43246
LukášMartinek <lukasmartinek97@gmail.com>
2025-04-04 10:49:04 +0200

    mobile_finish

--- menu.js
+++ menu.js
@@ -1,8 +1,27 @@
 function toggleMobileMenu() {
     let menu = document.getElementById("mobileMenu");
+    let body = document.body;
+
     if (menu.style.display === "block") {
         menu.style.display = "none";
+        body.style.overflow = ""; // Obnovíme scrollování
+        body.classList.remove("menu-open");
     } else {
         menu.style.display = "block";
+        body.style.overflow = "hidden"; // Zakážeme scrollování
+        body.classList.add("menu-open");
     }
 }
+
+// Funkce pro zavření menu při kliknutí na odkaz
+let menuLinks = document.querySelectorAll('.mobile-menu a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        let menu = document.getElementById("mobileMenu");
+        let body = document.body;
+
+        menu.style.display = "none"; // Zavřeme menu
+        body.style.overflow = ""; // Obnovíme scrollování
+        body.classList.remove("menu-open");
+    });
+});
